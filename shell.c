23b848411845a867df43b48795877b26bd14ce7d
Hasaan591gb <ahmad591gb@gmail.com>
2023-04-28 12:07:11 -0700

    simple shell 0.2

--- shell.c
+++ shell.c
@@ -6,7 +6,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-void execute_command(char *line);
+void execute_command(char **argv);
 
 /**
  * main - reads commands from standard input and executes them
@@ -16,8 +16,14 @@ void execute_command(char *line);
 int main(void)
 {
 	char *line = NULL;
+	char *lineptr = NULL;
+	char *delim = " \n";
 	size_t len = 0;
 	ssize_t nread;
+	char **argv;
+	char *token;
+	int num_token = 0;
+	int i;
 
 	while (1)
 	{
@@ -41,8 +47,34 @@ int main(void)
 		else if (strcmp(line, "\n") == 0)
 			continue;
 
-		if (line != NULL)
-			execute_command(line);
+		linecpy = malloc(sizeof(char) * nread);
+		if (linecpy == NULL)
+			return (1);
+
+		strcpy(linecpy, line);
+		token = strtok(linecpy, delim);
+		while (token != NULL)
+		{
+			num_token++;
+			token = strtok(NULL, delim);
+		}
+
+		argv = malloc(sizeof(char *) * (num_token + 1));
+		token = strtok(line, delim);
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+
+		execute_command(argv);
+
+		for (i = 0; i < num_token; i++)
+			free(argv[i]);
+		free(argv);
+		free(linecpy);
 	}
 
 	if (line != NULL)
@@ -52,20 +84,17 @@ int main(void)
 
 /**
  * execute_command - executes a command
- * @line: the command to execute
+ * @argv: the command to execute
  *
  * This function takes a command as input and executes it in a new process.
  * It uses the fork and execve functions to create a new process and execute
  * the command in the child process. The parent process waits for the child
  * process to complete using the wait function.
  */
-void execute_command(char *line)
+void execute_command(char **argv)
 {
-	char *argv[] = {NULL, NULL};
 	pid_t pid;
 
-	/* Tokenize the input to remove the newline character */
-	argv[0] = strtok(line, " \n");
 	if (argv[0] == NULL)
 	{
 		perror("Error: strtok failed\n");
